0cdf121a0b6c75a6e81f4b798751de25fdc036be
Duvan Vasquez <29andrey10@gmail.com>
2025-05-24 20:04:35 -0600

    Apply refactorization of code

--- Java/src/main/java/com/gildedrose/BLL_Products/cls_BLL_Products.java
+++ Java/src/main/java/com/gildedrose/BLL_Products/cls_BLL_Products.java
@@ -1,5 +1,93 @@
 package com.gildedrose;
 
+public class cls_BLL_Products {
+    Item[] items;
+
+    public cls_BLL_Products(Item[] items) {
+        this.items = items;
+    }
+
+/*
+La refactorización del código consiste en crear un método para cada
+acción con el fin de lograr un flujo más legible, comprensible 
+y mantenible. Se aplica la simplificación y la reutilización de métodos.
+*/
+
+    public void updateQuality(cls_DAL_Item product) {
+        for (Item item : items) {
+            updateItem(item);
+        }
+    }
+
+    private void updateItem(Item item) {
+        switch (item.name) {
+            case "Aged Brie":
+                updateAgedBrie(item);
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert":
+                updateBackstagePass(item);
+                break;
+            case "Sulfuras, Hand of Ragnaros":
+                // Sulfuras no cambia
+                break;
+            default:
+                updateStandardItem(item);
+        }
+    }
+
+     private void updateAgedBrie(Item item) {
+        increaseQuality(item);
+        item.sellIn--;
+
+        if (item.sellIn < 0) {
+            increaseQuality(item);
+        }
+    }
+
+     private void updateBackstagePass(Item item) {
+        increaseQuality(item);
+
+        if (item.sellIn <= 10) {
+            increaseQuality(item);
+        }
+
+        if (item.sellIn <= 5) {
+            increaseQuality(item);
+        }
+
+        item.sellIn--;
+
+        if (item.sellIn < 0) {
+            item.quality = 0;
+        }
+    }
+
+     private void updateStandardItem(Item item) {
+        decreaseQuality(item);
+        item.sellIn--;
+
+        if (item.sellIn < 0) {
+            decreaseQuality(item);
+        }
+    }
+
+/*Estos metodos son reutilizados para sumar y restar */
+
+    private void increaseQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality++;
+        }
+    }
+
+    private void decreaseQuality(Item item) {
+        if (item.quality > 0) {
+            item.quality--;
+        }
+    }
+}
+
+#region Old Code
+/*
 class GildedRose {
     Item[] items;
 
@@ -60,3 +148,5 @@ class GildedRose {
         }
     }
 }
+    */
+#endregion
---
+++ Java/src/main/java/com/gildedrose/DAL_Products/cls_DAL_Item.java
@@ -0,0 +1,50 @@
+package com.gildedrose;
+
+public class cls_DAL_Item {
+   
+    /* Se aplica el concepto de encapsulamiento para proteger 
+    los atributos de la clase */
+    private String _name;
+    private int _sellIn , _quality;
+
+    // Constructor
+    public Item(String name, int sellIn, int quality) {
+        this.name = name;
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
+
+    /* Getters y setters , se utilizan para acceder 
+    y modificar los atributos de la clase*/
+    public String name {get: return _name; set: _name = value;}
+    public int sellIn {get: return _sellIn; set: _sellIn = value;}
+    public int quality {get: return _quality; set: _quality = value;}
+
+
+    @Override
+    public String toString() {
+        return name + ", " + sellIn + ", " + quality;
+    }
+}
+
+#region Old Code
+/*public class Item {
+
+    public String name;
+
+    public int sellIn;
+
+    public int quality;
+
+    public Item(String name, int sellIn, int quality) {
+        this.name = name;
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
+
+   @Override
+   public String toString() {
+        return this.name + ", " + this.sellIn + ", " + this.quality;
+    }
+} */
+#endregion
